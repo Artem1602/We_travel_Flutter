de2949ebdc24018b7a92591c08e0d38aae6fe68d
Artem Nishchenko <artem.nishchenko0@gmail.com>
2021-06-27 23:25:19 +0300

    Add video list

--- lib/model/UserVideoTabModel.dart
+++ lib/model/UserVideoTabModel.dart
@@ -1,5 +1,18 @@
-import 'package:scoped_model/scoped_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:we_travel/model/WeTravelModel.dart';
 
-class UserVideoTabModel extends Model{
+final UserVideoTabModel userVideoTabModel =
+    new UserVideoTabModel(weTravelModel);
 
+class UserVideoTabModel extends WeTravelModel {
+  WeTravelModel weTravelModel;
+
+  List<Widget> userVideo = [];
+
+  UserVideoTabModel(this.weTravelModel);
+
+  void addVideoListItem(Widget widget){
+    userVideo.add(widget);
+    notifyListeners();
+  }
 }
--- lib/ui_tabs/myVideo/MyVideoTab.dart
+++ lib/ui_tabs/myVideo/MyVideoTab.dart
@@ -1,34 +1,87 @@
+import 'dart:typed_data';
+
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:video_thumbnail/video_thumbnail.dart';
+import 'package:we_travel/model/UserVideoTabModel.dart';
 import 'package:we_travel/model/WeTravelModel.dart';
-import 'package:we_travel/retrofit/RetrofitCore.dart';
-import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
-import 'package:we_travel/utils.dart';
 
-class MyVideoTab extends StatelessWidget{
-  final List<String> list = ["a","b","c","d"];
+class MyVideoTab extends StatelessWidget {
+  final List<String> list = [];
+
   @override
   Widget build(BuildContext context) {
     loadUserVideos();
-    return Expanded(
-      child: ListView.builder(
-        itemCount: list.length,
-        itemBuilder: (context,index){
-          return ListTile(
-            leading: Text("IMG"),
-            title: Text("${list[index]}"),
-          );
-        },
-      ),
-    );
+    return ScopedModel<UserVideoTabModel>(
+        model: userVideoTabModel,
+        child: ScopedModelDescendant(
+          builder: (BuildContext inContext, Widget inChild,
+              UserVideoTabModel inModel) {
+            return Expanded(
+                child: ListView.builder(
+                  itemExtent: 80,
+              itemCount: userVideoTabModel.userVideo.length,
+              itemBuilder: (context, index) {
+                return userVideoTabModel.userVideo[index];
+              },
+            ));
+          },
+        ));
   }
 
-  void loadUserVideos(){
+  void loadUserVideos() {
+    //TODO Test
+    VideoThumbnail.thumbnailData(
+            video:
+                "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4",
+            imageFormat: ImageFormat.JPEG,
+            quality: 20)
+        .then((value) => {
+              userVideoTabModel
+                  .addVideoListItem(buildListItem("fileRef.name", value))
+            });
 
-    firebase_storage.FirebaseStorage.instance.ref(weTravelModel.userID).listAll().then((value){
-      for(var fileRef in value.items){
-        String ss = fileRef.name;
-      }
-    });
+    // firebase_storage.FirebaseStorage.instance
+    //     .ref(weTravelModel.userID)
+    //     .listAll()
+    //     .then((value) {
+    //   for (var fileRef in value.items) {
+    //     if (fileRef.name == "profile_img") continue;
+    //     fileRef.getDownloadURL().asStream().listen((event) {
+    //       VideoThumbnail.thumbnailData(
+    //               video: event, imageFormat: ImageFormat.JPEG, quality: 20)
+    //           .then((value) => userVideoTabModel
+    //               .addVideoListItem(buildListItem(fileRef.name, value)));
+    //     });
+    //   }
+    // });
+  }
+
+  Widget buildListItem(String videoName, Uint8List img) {
+    return Card(
+      child: InkWell(
+        onTap: () {
+          //TODO handle click
+           },
+        child: Row(
+          children: [
+            Expanded(
+              flex: 30,
+              child: Image.memory(img),
+            ),
+            Expanded(
+                flex: 70,
+                child: Column(
+                  children: [
+                    Expanded(flex: 50, child: Text(videoName)),
+                    Expanded(flex: 50, child: Text("Info")),
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
